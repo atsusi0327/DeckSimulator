b1cc936084c41533fe35dea81fdf0d2b4e7988d4
atsusi0327 <atsusichenjp@gmail.com>
2025-07-10 14:00:38 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    google.accounts.id.initialize({
+      client_id: '73732306444-dfm8hcn3m7s8c1hgv5dv5pj6vsir819v.apps.googleusercontent.com',
+      callback: handleCredentialResponse
+    });
+
+    document.getElementById('google-login-btn').addEventListener('click', function () {
+      google.accounts.id.prompt();
+    });
+}
+
+function handleCredentialResponse(response) {
+    console.log('Encoded JWT ID token: ' + response.credential);
+    window.location.href = 'https://atsusi0327.github.io/DeckSimulator/home.html';
+  }
