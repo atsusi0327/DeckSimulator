61ef79c942c69cd20a46d2444f9a22544e851010
atsusi0327 <atsusichenjp@gmail.com>
2025-07-11 17:55:50 +0800

    init

--- js/zh-TW/decklist.js
+++ js/zh-TW/decklist.js
@@ -157,16 +157,20 @@ async function refreshDeckList() {
     for (const deck of decks) {
         const appendstr = `
         <li class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3">
-            <div class="card h-100">
+            <div class="card">
                 <div class="card-body position-relative pt-card">
-                    <button class="btn btn-outline-light border-0 shadow-none outline-0 d-flex justify-content-center align-items-center position-absolute top-0 end-0 bottom-0 start-0 p-0 m-0" type="button" onclick="editDeck('${deck.id}');">
-                        <p class="m-0 text-truncate w-100 text-center px-2">
-                            ${deck.name}
-                        </p>
+                    <button class="btn btn-outline-light border-0 shadow-none outline-0 d-flex flex-column position-absolute top-0 end-0 bottom-0 start-0 p-0 m-0" type="button">
+                        <div class="card-details flex-1 d-flex justify-content-center align-items-center">
+                            <p class="text-center m-0">
+                                ${deck.name}
+                            </p>
+                        </div>
+                        <div class="card-createdt">
+                            <p class="small text-muted text-center m-0">
+                                ${deck.created}
+                            </p>
+                        </div>
                     </button>
-                </div>
-                <div class="card-footer text-center small text-muted">
-                    ${deck.created}
                 </div>
             </div>
         </li>`;
